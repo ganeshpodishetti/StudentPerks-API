cee72228812fe0315f8aff52b56dfc2b9e340420
Ganesh Podishetti <ganeshpodishetti@gmail.com>
2025-07-03 17:06:23 -0400

    feat: Change properties in ConnStringOptions and JwtOptions to use set accessors

--- backend/src/SP.Domain/Options/JwtOptions.cs
+++ backend/src/SP.Domain/Options/JwtOptions.cs
@@ -3,9 +3,9 @@ namespace SP.Domain.Options;
 public class JwtOptions
 {
     public const string Jwt = "Jwt";
-    public string Issuer { get; init; } = null!;
-    public string Audience { get; init; } = null!;
-    public string Key { get; init; } = null!;
-    public double AccessTokenExpirationInMinutes { get; init; }
-    public int RefreshTokenExpirationInDays { get; init; }
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string Key { get; set; } = null!;
+    public double AccessTokenExpirationInMinutes { get; set; }
+    public int RefreshTokenExpirationInDays { get; set; }
 }
